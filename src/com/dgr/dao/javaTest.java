5446319e6cdf18dd3a699d74aa43c45e483ec6e6
DgrRelax <13723762475@163.com>
2019-02-15 22:04:52 +0800

    测试提交

--- src/com/dgr/dao/javaTest.java
+++ src/com/dgr/dao/javaTest.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class javaTest {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		String URL = "jdbc:mysql://localhost:3306/sakila?useUnicode=true&characterEncoding=utf-8&useSSL=false&serverTimezone=UTC";
+		String URL = "jdbc:mysql://localhost:3306/datasource?useUnicode=true&characterEncoding=utf-8&useSSL=false&serverTimezone=UTC";
 		String USER = "root";
 		String PASSWORD = "root";
 		//1.load dirve
